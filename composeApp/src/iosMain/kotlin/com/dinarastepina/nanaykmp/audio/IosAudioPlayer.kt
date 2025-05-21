995a0a3434db5f968e9f295ab99bf1c0498984d8
Dinara Stepina <setare497@gmail.com>
2025-05-21 19:05:37 +0300

    added player implementation for ios

--- composeApp/src/iosMain/kotlin/com/dinarastepina/nanaykmp/audio/IosAudioPlayer.kt
+++ composeApp/src/iosMain/kotlin/com/dinarastepina/nanaykmp/audio/IosAudioPlayer.kt
@@ -1,16 +1,31 @@
 package com.dinarastepina.nanaykmp.audio
 
-import com.dinarastepina.nanaykmp.audio.AudioPlayer
-import com.dinarastepina.nanaykmp.audio.AudioResourceManager
 import com.dinarastepina.nanaykmp.data.toNSData
 import kotlinx.cinterop.ExperimentalForeignApi
 import kotlinx.coroutines.runBlocking
 import platform.AVFAudio.AVAudioPlayer
+import platform.AVFAudio.AVAudioSession
+import platform.AVFAudio.AVAudioSessionCategoryPlayback
+import platform.AVFAudio.setActive
 
 
 class IosAudioPlayer : AudioPlayer {
     private var audioPlayer: AVAudioPlayer? = null
     private var currentDataSource: ByteArray? = null
+
+    init {
+        configureAudioSession()
+    }
+
+    @OptIn(ExperimentalForeignApi::class)
+    private fun configureAudioSession() {
+        try {
+            AVAudioSession.sharedInstance().setCategory(AVAudioSessionCategoryPlayback, error = null)
+            AVAudioSession.sharedInstance().setActive(true, null)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
+    }
 
     override fun play() {
         audioPlayer?.play()
@@ -25,9 +40,15 @@ class IosAudioPlayer : AudioPlayer {
         audioPlayer?.currentTime = 0.0
     }
 
+    @OptIn(ExperimentalForeignApi::class)
     override fun release() {
         audioPlayer = null
         currentDataSource = null
+        try {
+            AVAudioSession.sharedInstance().setActive(false, null)
+        } catch (e: Exception) {
+            e.printStackTrace()
+        }
     }
 
     @OptIn(ExperimentalForeignApi::class)
